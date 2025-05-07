8dfda9878f8523eb91398ac714048872ffce5385
batumaru12 <taku02141212@gmail.com>
2025-05-07 17:19:13 +0900

    refactoring plot.py

--- plot.py
+++ plot.py
@@ -2,15 +2,12 @@ import json
 import matplotlib.pyplot as plt
 import os
 
-# ログファイルのパス
 log_file_path = "./amed/train_weights/new_data_mae/log.txt" #結果を保存したディレクトリのlog.txtを参照
 output_dir = "./amed/output/new_data_plot/" #プロット結果を出力するフォルダ
 
-#指定した出力フォルダがない場合作成
 if not os.path.isdir(output_dir):
     os.makedirs(output_dir)
 
-# データを格納するリスト
 epochs = []
 train_lr = []
 train_loss = []
@@ -21,7 +18,6 @@ train_loss_giou = []
 test_loss_giou = []
 test_coco_eval_bbox = []
 
-# ログファイルの読み込みと解析
 with open(log_file_path, 'r') as log_file:
     for line in log_file:
         try:
@@ -38,7 +34,6 @@ with open(log_file_path, 'r') as log_file:
         except json.JSONDecodeError:
             continue
 
-# グラフ描画と保存
 def save_plot(x, y, xlabel, ylabel, title, filename):
     """指定されたデータをプロットしてPNGファイルに保存"""
     plt.figure()
@@ -52,22 +47,16 @@ def save_plot(x, y, xlabel, ylabel, title, filename):
     plt.savefig(filename)
     plt.close()
 
-#トレーニング学習率
 save_plot(epochs, train_lr, "Epochs", "train_lr", "train learning rate", output_dir + "use_mae_train_lr.png") 
 
-# トレーニング損失
 save_plot(epochs, train_loss, "Epochs", "Loss", "Training Loss", output_dir + "use_mae_train_loss.png")
 
-# テスト損失
 save_plot(epochs, test_loss, "Epochs", "Loss", "Test Loss", output_dir + "use_mae_test_loss.png")
 
-# バウンディングボックス損失
 save_plot(epochs, train_loss_bbox, "Epochs", "BBox Loss", "Training Bounding Box Loss", output_dir + "use_mae_train_bbox_loss.png")
 save_plot(epochs, test_loss_bbox, "Epochs", "BBox Loss", "Test Bounding Box Loss", output_dir + "use_mae_test_bbox_loss.png")
 
-# GIoU損失
 save_plot(epochs, train_loss_giou, "Epochs", "GIoU Loss", "Training GIoU Loss", output_dir + "use_mae_train_giou_loss.png")
 save_plot(epochs, test_loss_giou, "Epochs", "GIoU Loss", "Test GIoU Loss", output_dir + "use_mae_test_giou_loss.png")
 
-# COCO評価（適合率・再現率）
 save_plot(epochs, test_coco_eval_bbox, "Epochs", "COCO Eval (Precision/Recall)", "COCO Evaluation Metrics", output_dir + "use_mae_coco_eval.png")
