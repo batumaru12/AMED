f71e46b11124ffc09c0107b66e2fcf751fbb9d04
batumaru12 <taku02141212@gmail.com>
2025-06-08 15:31:41 +0900

    chage

--- train.py
+++ train.py
@@ -83,7 +83,6 @@ def get_args_parser():
     # dataset parameters
     parser.add_argument('--dataset_file', default='coco')
     parser.add_argument('--coco_path', default="./coco", type=str)
-    parser.add_argument('--annotations_path', default="./coco", type=str)
     parser.add_argument('--coco_panoptic_path', type=str)
     parser.add_argument('--remove_difficult', action='store_true')
 
